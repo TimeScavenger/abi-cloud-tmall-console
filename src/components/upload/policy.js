23e0dc2bd4f8d272767a7582a5d7f5b4fedac0c8
YunleiSun <v_YunleiSun@budweiserapac.com>
2021-10-31 19:40:07 +0800

    上传图片的接口进行调整

--- src/components/upload/policy.js
+++ src/components/upload/policy.js
@@ -3,7 +3,7 @@ import http from '@/utils/httpRequest.js'
 export function policy () {
   return new Promise((resolve, reject) => {
     http({
-      url: http.adornUrl('/aliyun/aliyunoss/policy'),
+      url: http.adornUrl('/basic/aliyunoss/policy'),
       method: 'get',
       params: http.adornParams({})
     }).then(({data}) => {
