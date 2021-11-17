817510f85982ecc55802703aeea9d3d2d1849b29
YunleiSun <v_YunleiSun@budweiserapac.com>
2021-11-17 22:50:14 +0800

    商品服务：上传图片的接口前后端联调

--- src/components/upload/policy.js
+++ src/components/upload/policy.js
@@ -3,7 +3,7 @@ import http from '@/utils/httpRequest.js'
 export function policy () {
   return new Promise((resolve, reject) => {
     http({
-      url: http.adornUrl('/basic/aliyunoss/policy'),
+      url: http.adornUrl('/basic/oss/file-upload-policy'),
       method: 'get',
       params: http.adornParams({})
     }).then(({data}) => {
